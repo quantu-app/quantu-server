e4635e7016b27676e08393dfc197ae085ef7461c
Nathan Faucett <nathanfaucett@gmail.com>
2023-01-12 08:07:55 -0500

    send more user info back

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@
 module Api
   class UsersController < BaseController
     def me
-      render(json: @current_user.as_json(only: %i[username email]), status: :ok)
+      render(json: @current_user.as_json(only: %i[id username email created_at updated_at]), status: :ok)
     end
   end
 end
