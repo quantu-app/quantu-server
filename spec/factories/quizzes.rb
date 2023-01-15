c2589909bbe58d61844ad68d8aad7f7df56e4c20
John Faucett <jwaterfaucett@gmail.com>
2023-01-15 13:11:19 +0100

    Make quiz names very unique

--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :quiz do
-    name { Faker::Book.title }
+    name { "#{Faker::Book.title}-#{Faker::Internet.uuid}" }
+    uri { Faker::Internet.uuid }
 
     association :user
   end
